4d7101ac22bcea4c06a66564d8100f3e8ab28a95
Mursid S <97183746+Botwa021@users.noreply.github.com>
2022-07-03 00:38:36 +0700

    Update info-creator.js

--- plugins/info-creator.js
+++ plugins/info-creator.js
@@ -3,29 +3,29 @@ let handler = async (m, {conn}) => {
 const vcard = `BEGIN:VCARD
 VERSION:3.0
 N:;;;
-FN: ᴹᴿ᭄ ZifaOffcོ_1ঔৣ꧂
-item.ORG: ᴹᴿ᭄ ZifaOffcོ_1ঔৣ꧂
-item1.TEL;waid=6285828377610:6285828377610@s.whatsapp.net
+FN: .
+item.ORG: .
+item1.TEL;waid=6288233832771:6288233832771@s.whatsapp.net
 item1.X-ABLabel:owner selalu ada 
-item2.EMAIL;type=INTERNET: anidaanida59@gmail.com
+item2.EMAIL;type=INTERNET: mursidmulo@gmail.com
 item2.X-ABLabel:📧Email
-item3.ADR:;;🎍Pontianak;;;;
+item3.ADR:;;🎍Yogyakarta;;;;
 item3.X-ABADR:ac
-item3.X-ABLabel:asal kota
-item4.URL:https//github.com/Botynyakamu
+item3.X-ABLabel:ASAL KOTA
+item4.URL:https//github.com/Botwa021
 item4.X-ABLabel:Website
 END:VCARD`
 const sentMsg  = await conn.sendMessage(
     m.chat,
     { 
         contacts: { 
-            displayName: 'OWNER BOT', 
+            displayName: 'owner', 
             contacts: [{ vcard }]  
         }
     }
 )
-let rozi = 'https://telegra.ph/file/6de3ca8fe283f2e236ec4.jpg'
-await conn.send3ButtonImg(m.chat, rozi, "*Hai kak👋,  Itu nomor owner gw…*", '📮: jngn di spam kack', 'Source code', '.sc', 'Back', '.menu', 'Donasi', '.donasi', sentMsg)}
+let rozi = 'https://telegra.ph/file/f8a1b984a287fa705fd7a.jpg'
+await conn.send3ButtonImg(m.chat, rozi, "*Hai kak👋,  Itu nomor ownerku*", '📮: jan spam ya', 'Source code', '.sc', 'Back', '.menu', 'Donasi', '.donasi', sentMsg)}
 handler.help = ['owner2', 'creator2']
 handler.tags = ['info']
 
