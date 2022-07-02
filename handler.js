49210cd6316f4c56b0736c3f47e342b2edd1931f
Mursid S <97183746+Botwa021@users.noreply.github.com>
2022-07-03 00:34:01 +0700

    Update handler.js

--- handler.js
+++ handler.js
@@ -808,7 +808,7 @@ global.dfail = (type, m, conn) => {
  let unreg = {
   unreg: `
 ┏━━━〔 ıll 𝐑𝐄𝐆𝐈𝐒𝐓𝐄𝐑 llı 〕━━❑
-⬡ Hallo mypren👋, @${m.sender.split`@`[0]}
+⬡ Hallo 👋, @${m.sender.split`@`[0]}
 ⬡ Sebelum melihat fitur bot, lebih baik register dulu
 ⬡ Kalau tidak kelihatan button nya, contohnya dibawah!
 ┗━━━━━━━━━━━━━━━━━━❑
